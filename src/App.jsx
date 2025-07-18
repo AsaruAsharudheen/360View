43657c5efd9fdd83528a3cac0be149296f171d56
AsaruAsharudheen <asaru313313@gmail.com>
2025-07-18 12:43:40 +0530

    Your update message

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div className="full-screen-iframe">
       <iframe
-        src="https://momento360.com/e/u/3ae2432dd56349f1bce08ddec83a9847?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true"
+        src="https://momento360.com/e/u/34cb55e9e4824d1cbc5cad9dd518d22a?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true"
         frameBorder="0"
         allowFullScreen
         title="360 View"
