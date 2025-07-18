bea6592d70744a8fbce17b839f36d4bb32c532b4
AsaruAsharudheen <asaru313313@gmail.com>
2025-07-18 10:11:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="full-screen-iframe">
+      <iframe
+        src="https://momento360.com/e/u/3ae2432dd56349f1bce08ddec83a9847?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true"
+        frameBorder="0"
+        allowFullScreen
+        title="360 View"
+      ></iframe>
+    </div>
+  );
+};
+
+export default App;
