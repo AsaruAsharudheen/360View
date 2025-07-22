851686672fc98e481820ed6505db237e76f3a4c9
AsaruAsharudheen <asaru313313@gmail.com>
2025-07-22 12:25:18 +0530

    Your update message

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './Home';
-import Add360Url from './Add360Url';
 
 const App = () => {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/add" element={<Add360Url />} />
-      </Routes>
-    </>
+    <div className="full-screen-iframe">
+      <iframe
+        src="https://momento360.com/e/u/34cb55e9e4824d1cbc5cad9dd518d22a?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true"
+        frameBorder="0"
+        allowFullScreen
+        title="360 View"
+      ></iframe>
+    </div>
   );
 };
 
